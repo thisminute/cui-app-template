da25d30148655ddf8d0be6d9f937fa316c8fe933
Timofey Kondrashov <tkondrashov@gmail.com>
2020-07-14 21:01:14 -0400

    use new macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use cwf::cwf;
+use cwf::{ cwf, cwf_bindgen };
 extern crate console_error_panic_hook;
 
 // std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-cwf!();
+cwf_bindgen!();
