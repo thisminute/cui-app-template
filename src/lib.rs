8f74bf35c66fe80c5725a71a4a4941b0dea650c4
Sasha <sasha@thisminute.org>
2020-08-06 01:53:32 -0400

    support generated index.html page

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,5 @@ extern crate console_error_panic_hook;
 // std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
 cwl! {
-   title: "hello";
-
-   .text {
-      text: "hello";
-   }
-
-   text {
-      text: "world";
-      link: "http://google.com";
-      text {
-         background_color: "gray";
-      }
-   }
+   title: "template";
 }
