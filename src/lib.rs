19ca25ee78d46043cfe11692d7b515449f0ec153
Sasha <sasha@thisminute.org>
2022-03-30 04:15:39 -0400

    split app template from dev server + potential other tools

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,8 @@
 use cascading_ui::cui;
 
 cui! {
-	title: "hello world";
-
-	text: "this is a demo of some implemented features";
-
-	.a {
-		color: "blue";
-	}
-	a {
-		text: "clicking me does nothing";
-		.b {
-			color: "green";
-		}
-		b {
-			?click {
-				text: "hi";
-			}
-			text: "click me to say hi";
-		}
-	}
-	a {
-		text: "clicking me makes a new element";
-		?click {
-			b {
-				text: "hello world!";
-			}
-		}
+	text: "click me";
+	?click {
+		text: "hello world";
 	}
 }
